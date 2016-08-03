bbfe8d1724e425e72e1af74d0a6de80dbbe9168c
17khba <17khbaboy@gmail.com>
2016-08-03 13:38:30 +0800

    文件更新

--- task26/js/index.js
+++ task26/js/index.js
@@ -1,52 +1,52 @@
-var screenWidth, screenHeight, planetRadiu, screenCenterX, screenCenterY, currentX, currentY, spaceship_count, spaceship_size, spaceship_speed, default_charge_rate, default_discharge_rate, failture_rate, oImg, powerBar_pos_offset;
+var screenWidth, screenHeight, shipWidth, charge_rate, discharge_rate;
 
-screenWidth = 800;						//画布宽度
-screenHeight = 800;						//画布高度
-planetRadiu = 50;						//圆半径
-screenCenterX = 800 / 2;				//中心x
-screenCenterY = 800 / 2;				//中心y
-currentX = screenCenterX - 50;			//350
-currentY = screenCenterY - 50;			//350
-spaceship_count = 4;					//飞机数量
-spaceship_size = 40;					//飞机大小	
-spaceship_speed = 2;					//飞行速度
-default_charge_rate = 0.3;				//充电速度
-default_discharge_rate = 0.2;			//放电速度
-failture_rate = 0.3;					//消息发送失败率
-powerBar_pos_offset = 5;				//电量条偏移位置
+screenWidth = 800;					//画布宽高
+screenHeight = 800;
+screenCenterX = 400;				//画布中心位置
+shipWidth = 40;						//飞船宽
+shipSpeed = 2;						//飞船飞行速度
+charge_rate = 0.3;					//充电速度
+discharge_rate = 0.2;				//掉电速度
+
+planetRadiu = 50;					//星球半径
+
+powerBar_pos_offset = 5;			//电量条便宜位置
+powerBra_width = 5;					//电量条宽度
+powerBar_good = '#7DE8B7';			//电量充足时的颜色
+powerBar_medium = '#FFB22E';		//电量一般时的颜色
+powerBar_bad = '#FF3900';			//电量不足时的颜色
+
 
 window.requestAnimationFrame = window.requestAnimationFrame || window.mozRequestAnimationFrame || window.webkitRequestAnimationFrame || window.msRequestAnimationFrame;
 
 
-
 var Spaceship = function( id ){
-	this.id = id;				//飞机id
-	this.power = 100;			//初始电量
-	this.currentState = 'stop'; //初始状态
-	this.mediator = null;		//初始化时未安装喇叭
-	this.orbit = 100 + 50 * id - spaceship_size / 2;	//飞机所在轨道的半径
-	this.deg = 0;				//飞机初始角度
-	this.timer = null;
+	this.id = id;
+	this.curStatus = 'stop';						//飞船初始状态
+	this.power = 100;								//飞船初始能量值
+	this.deg = 0;									//飞船初始角度为0
+	this.orbit = 100 + 50 * id - shipWidth / 2;		//飞船所在轨道半径
+	this.mediator = null;							//初始化时未安装喇叭
 };
 
 /**
- * [dynamicManager 飞船动力系统]
- * @return {[obj]} [obj = { fly, stop }]
+ * [angleManager 控制飞船角度状态的改变]
+ * @return {[type]} [description]
  */
-Spaceship.prototype.dynamicManager = function(){
-
+Spaceship.prototype.angleManager = function(){
 	var self = this;
+
 	var fly = function(){
 		self.timer = setInterval( function(){
-			self.deg += spaceship;
+			self.deg += shipSpeed;
 			if( self.deg >= 360 ) self.deg = 0;
-		}, 20 );
-		ConsoleMs.show( 'Spaceship No.' + self.id + ' is flying.');
+		}, 16 );
+		ConsoleMs.show( 'Spaceship No.' + self.id + ' is flying.' );
 	};
 
 	var stop = function(){
 		clearInterval( self.timer );
-		ConsoleMs.show( 'Spaceship No.' + self.id + ' is stop.' );
+		ConsoleMs.show( 'Spaceship No.' + self.id + ' has stop.' );
 	};
 
 	return {
@@ -56,13 +56,18 @@ Spaceship.prototype.dynamicManager = function(){
 
 };
 
+/**
+ * [powerManager 控制电量条状态的改变]
+ * @return {[type]} [description]
+ */
 Spaceship.prototype.powerManager = function(){
-	var self  = this;
 
+	var self = this;
+
+	//充电
 	var charge = function(){
-		var chargeRate = default_charge_rate;
 		var timer = setInterval( function(){
-			if( self.currentState === 'fly' || self.currentState === 'destory' ){
+			if( self.curStatus === 'fly' || self.curStatus === 'destory' ){
 				clearInterval( timer );
 				return false;
 			}
@@ -71,67 +76,63 @@ Spaceship.prototype.powerManager = function(){
 				self.power = 100;
 				return false;
 			}
-			self.power += chargeRate;
-			return true;
-		}, 20 );
+			self.power += charge_rate;
+		}, 16 );
 		ConsoleMs.show( 'Spaceship No.' + self.id + ' is charging.' );
 	};
 
+	//掉电
 	var discharge = function(){
-		var dischargeRate = default_discharge_rate;
 		var timer = setInterval( function(){
-			if( self.currentState === 'stop' || self.currentState === 'destory' ){
+			if( self.curStatus === 'stop' || self.curStatus === 'destory' ){
 				clearInterval( timer );
 				return false;
 			}
 			if( self.power <= 0 ){
 				clearInterval( timer );
 				self.power = 0;
-				self.stateManager.changeState( 'stop' );
+				self.statusSys().changeState( 'stop' );
 				return false;
 			}
-			self.power -= dischargeRate;
-		}, 20 );
-		ConsoleMs.show( 'Spaceship No.' + self.id + ' is discharging.' );
+			self.power -= discharge_rate;
+		}, 16 );
+		ConsoleMs.show( 'Spaceship No.' + self.id + ' is dischargin.' );
 	};
 
 	return {
 		charge: charge,
-		discharge :discharge
+		discharge: discharge
 	};
 
 };
 
 /**
- * [stateManager 状态管理]
- * @return {[obj]} [obj = { changeState: changeState]
+ * [statusSys 飞船状态管理]
+ * @return {[obj]} [扔出一个可以控制状态的对象]
  */
-Spaceship.prototype.stateManager = function(){
+Spaceship.prototype.statusSys = function(){
+	var self = this;
 
-	var self, states, changeState;
-	self = this;
-	states = {
-		fly: function( state ){
-			self.currentState = 'fly';					//改变当前状态
-			self.dynamicManager().fly();				//控制飞船的飞行
-			self.powerManager().discharge();			//飞船放电
+	var status = {
+		fly: function( cmd ){
+			self.curStatus = cmd;
+			self.angleManager().fly();
+			self.powerManager().discharge();
 		},
-		stop: function( state ){
-			self.currentState = 'stop';
-			self.dynamicManager().stop();
+		stop: function( cmd ){
+			self.curStatus = cmd;
+			self.angleManager().stop();
 			self.powerManager().charge();
 		},
-		destroy: function( state ){
-			self.currentState = 'destroy';
-			self.mdiator.remove( self );				//销毁飞船
+		destory: function( cmd ){
+			self.curStatus = cmd;
+			self.mediator.remove( self );
 		}
 	};
 
-	changeState = function( state ){
-
-		states[ state ] && states[ state ]();
-		ConsoleMs.show( 'Spaceship NO.' + self.id + ' state is ' + state );
-
+	var changeState = function( cmd ){
+		status[ cmd ] && status[ cmd ]( cmd );
+		ConsoleMs.show( 'Spaceship No.' + self.id + ' status is ' + cmd );
 	};
 
 	return {
@@ -141,297 +142,324 @@ Spaceship.prototype.stateManager = function(){
 };
 
 /**
- * [signalManage 信号系统]
- * @return {[obj]} [obj = { receive: function(){} }]
+ * [singelSys 飞船信息系统]
+ * @param  {[obj]} msg [对应的属性为id和cmd]
+ * @return {[type]}     [description]
  */
-Spaceship.prototype.signalManage = function(){
-	var self = this;					//sapceship的实例
+Spaceship.prototype.singelSys = function( msg ){
+	var self = this;
 	return {
 		receive: function( msg ){
-			if( self.currentState !== msg.cmd && self.id === msg.id ){
-				self.stateManager().changeState( msg.cmd );
+			//处于循环中，得判断用户点击的对象
+			if( self.curStatus !== msg.cmd && self.id === msg.id ){
+				self.statusSys().changeState( msg.cmd );
 			}
 		}
 	};
 };
 
-
 /**
- * [Commander 指挥官]
- * 单向传输指令，只负责发出指令
+ * [Commander 指挥官，负责单向指令传输，不接收信息]
  */
 var Commander = function(){
-	this.id = 'Don';					
-	this.cmds = [];						//命令集
-	this.mediator = null;				//中间信息载体(喇叭)，为obj对象
+	this.id = 'Don';
+	this.mediator = null;					//指挥官的喇叭，初始化时未安装
 };
 
 /**
- * [send 命令下达]
- * @param  {[obj]} msg [obj.id为对应按钮的index值，obj.cmd为下达的命令]
+ * [send 指挥官发出的指令]
+ * @param  {[obj]} msg [对应的属性为id和cmd]
  */
 Commander.prototype.send = function( msg ){
-	this.mediator.send( msg );
-	this.cmds.push( msg );
+	this.mediator.send( msg );				//指挥官用自己的喇叭对象发出指令
 };
 
 /**
- * [Mediator 中间消息载体(喇叭)]
- * @return {[obj]}         [喇叭绑定的功能---注册、发送、创建]
+ * [Mediator 喇叭，用于数据传输]
  */
 var Mediator = function(){
-	this.spaceships = [];				//用于存放飞船实例
-	this.commander = null;				//指挥官实例
+	var spaceships = [];					//私有属性，保存spaceship实例对象
+	var commander = null;					//喇叭的所属者
+
 	return {
-		// 对象必须先注册才能进行通讯
+
+		/**
+		 * [registor 指挥官、飞船必须注册才能进行通讯]
+		 * @param  {[obj]} obj [注册的对象]
+		 * @return {[boolean]}     [注册成功返回true，反之false]
+		 */
 		registor: function( obj ){
 			if( obj instanceof Commander ){
-				commander = obj;
-				obj.mediator = this;
-				ConsoleMs.show( 'mediator registor' + 'commander ' + obj.id );
+				commander = obj;			//指定喇叭的所属者
+				obj.mediator = this;		//给指挥官安装喇叭
+				ConsoleMs.show( 'Commander installed speaker！');
 				return true;
 			} else if( obj instanceof Spaceship ){
 				spaceships[ obj.id ] = obj;
 				obj.mediator = this;
-				ConsoleMs.show( 'mediator registor spaceship' + obj.id );
+				ConsoleMs.show( 'Spaceship installed speaker！' );
 				return true;
 			}
-			ConsoleMs.show( 'mediator registor failed' );
+			ConsoleMs.show( 'registor failed！' );
 			return false;
 		},
 
-		send: function( msg ){				//obj = { id, cmd };
-			var self, success;
+		/**
+		 * [send 消息的发出]
+		 * @param  {[obj]} msg [消息对象，包含id和cmd两个属性]
+		 */
+		send: function( msg ){
+			var self ,success;
 			self = this;
-			setTimeout( function(){
-				success = Math.random() > failture_rate ? true : false;
-				if ( !success ){
-					ConsoleMs.show( 'send failed' );
-					return false;
-				}
-				if ( msg.cmd === 'launch' ){			
-					self.create( msg );
-				}
-				ConsoleMs.show ( 'send success' );
-				return true;
-			}, 1000 );
+			success = Math.random() > 0.3 ? true : false;
+			if( success ){
+				setTimeout( function(){
+					if( msg.cmd === 'launch' ){
+						self.create( msg.id );
+					}
+					// 循环操作存储飞船实例的数组，发送消息
+					for( var key in spaceships ){
+						if( spaceships[ key ] !== undefined ){
+							spaceships[ key ].singelSys().receive( msg );
+						}
+					}
+				}, 1000 );
+				ConsoleMs.show( 'Message send success.' );
+			} else{
+				ConsoleMs.show( 'Message send failed.' );
+			}
 		},
 
-		create: function( msg ){
+		/**
+		 * [create 飞船的创建及喇叭的安装]
+		 * @param  {[num]} id [飞船对应id号]
+		 */
+		create: function( id ){
+			var spaceship = new Spaceship( id );
+			this.registor( spaceship );			//给飞船安装喇叭
+		},
 
-			var spaceship;
-
-			if ( spaceships [ msg.id ] !== undefined ){
-				ConsoleMs.show( 'Spaceship already exists' );
+		/**
+		 * [remove 移除通讯对象]
+		 * @param  {[obj]} obj [飞船对象实例]
+		 */
+		remove: function( obj ){
+			if( obj instanceof Spaceship ){
+				ConsoleMs.show( 'Destory spaceship success.' );
+				delete this.spaceships[ obj.id ];
 				return false;
 			}
-
-			spaceship = new Spaceship( msg.id );		//新建飞机实例
-			this.registor( spaceship );					//给飞机装个喇叭
-			return true;
-
+			ConsoleMs.show( 'Destory spaceship success.' );
 		},
 
+		/**
+		 * [getSpaceships 获取飞船队列]
+		 * @return {[arr]} [飞船队列]
+		 */
 		getSpaceships: function(){
-			return sapceships;
+			return spaceships;
 		}
-
 	};
-
 };
-
 
 /**
- * [Message 消息载体，只负责将信息转换成以实例输出]
- * @param {[num]} target  [对应的index值]
- * @param {[str]} command [命令信息]
+ * [Message 消息载体，将数据转换成对象]
+ * @param {[num]} num [对应spaceship的num值]
+ * @param {[type]} str [操作名称]
  */
-var Message = function( target, command){
-	this.id = target;
-	this.cmd = command;
+var Message = function( num, str ){
+	this.id = num;
+	this.cmd = str;
 };
 
-var btnListener = function(){
-	var id, cmd;
-	id = null;
-	cdm = null;
-	$('.btns').on('click', function(){
-		var cmdName = $(this).parent().attr('name');
+/**
+ * [btnListener 按钮操作监听]
+ * @param  {[obj]} commander [指挥官，用于发送消息的对象]
+ */
+var btnListener = function( commander ){
+	var id, cmd, message;
+	$('.control button').on('click', function(){
 		id = $(this).parent().index();
-		cmd = cmdName;
+		cmd = $(this).text();
+		message = new Message( id, cmd );
+		commander.send( message );				//指挥官发送指令
 	});
-	var message = new Message( id, cdm );
-	commander.send( message );
-	return true;
 };
 
-
 /**
- * [description]
- * @param  {[obj]} 	msg	   [msg = { id, cmd }]
- * @return {[obj]}         [返回添加显示信息的方法]
+ * [动画工具---渲染动画，用缓存画布内容代替真实画布内容]
  */
-var ConsoleMs = (function(){
-	var consoleLog = $('.console ul');
-	var show = function( msg ){
-		var oLi = $('<li/>');
-		oLi.text( msg );
-		consoleLog.prepend( oLi );
-	};
-	return {
-		show: show
-	};
-})();
+var AnimTool = ( function(){
+	var canBg, ctx, canPlanet, cacheCtx, mediator;
+	//真实画布
+	canBg = document.querySelector( '.planet' );
+	canBg.width = screenWidth;
+	canBg.height = screenHeight;
+	ctx = canBg.getContext( '2d' );
 
-var AnimUtil = ( function(){
-	var canva, ctx, cacheCanva, cacheCtx, mediator;
-	mediator = null;
-	// 接受填充的画布（真实渲染用的画布）
-	canva = document.querySelector( '#screen' );
-	canva.width = screenWidth;
-	canva.height = screenHeight;
-	ctx = canva.getContext( '2d' );
+	//缓存画布
+	canPlanet = document.createElement( 'canvas' );
+	canPlanet.width = screenWidth;
+	canPlanet.height = screenHeight;
+	cacheCtx = canPlanet.getContext( '2d' );
 
-	// 缓存画布（暂存）
-	cacheCanva = document.createElement( 'canva' );
-	cacheCanva.width = screenWidth;
-	cacheCanva.height = screenHeight;
-	cacheCtx = cacheCanva.getContext( '2d' );
+	mediator = null;						//传输介质，用于获取spaceships数组的值
 
 	/**
 	 * [drawPlanet 画行星]
-	 * @param  {[obj]} ctx [canvas目标画布]
+	 * @param  {[obj]} _ctx [canvas对象]
 	 */
-	var drawPlanet = function ( ctx ){
+	var drawPlanet = function( _ctx ){
 
+		var x, oImg;
+		x = screenCenterX - planetRadiu;			//350,绘制起点
 		oImg = new Image();
-
-		oImg.onload = function (){
-
-			ctx.drawImage( oImg, currentX, currentY, planetRadiu * 2, planetRadiu * 2 );
+		oImg.onload = function(){
+			_ctx.drawImage( oImg, x, x, planetRadiu * 2, planetRadiu * 2 );
 		};
-
-		oImg.src = 'img/oreo.png';
+		oImg.src = './img/oreo.png';
 
 	};
 
 	/**
-	 * [drawOrbit 画飞船轨道]
-	 * @param  {[obj]} ctx [canvas目标画布]
+	 * [drawOrbit 画行星轨道]
+	 * @param  {[obj]} _ctx [canvas对象]
 	 */
-	var drawOrbit = function ( ctx ){
-
-		for (var i = 0; i < planteCount; i++) {
-			
-			ctx.strokeStyle = '#999';
-			ctx.beginPath();
-			ctx.arc( screenCenterX, screenCenterY, 100 + 50 * i, 0, 2 * Math.PI );
-			ctx.closePath();
-			ctx.stroke();
-
+	var drawOrbit = function( _ctx ){
+		for (var i = 0; i < 4; i++) {
+			_ctx.strokeStyle = '#999';
+			_ctx.beginPath();
+			_ctx.arc( screenCenterX, screenCenterX, 100 + 50 * i, 0, 2 * Math.PI, false );
+			_ctx.closePath();
+			_ctx.stroke();
 		}
-
 	};
 
+	/**
+	 * [初始化时渲染bg，渲染动画时则不再重复操作]
+	 */
+	(function(){
+		var canva, ctx;
+		canva = document.querySelector( '.screen' );
+		canva.width = screenWidth;
+		canva.height = screenHeight;
+		ctx = canva.getContext( '2d' );
+
+		drawPlanet( ctx );
+		drawOrbit( ctx );
+	})();
+
+	/**
+	 * [drawSpaceship 画飞船]
+	 * @param  {[obj]} _ctx      [缓存画布对象]
+	 * @param  {[obj]} spaceship [飞船对象]
+	 */
+	var drawSpaceship = function( _ctx, spaceship ){
+		var oImg = new Image();
+		oImg.onload = function(){
+			try{
+				// 绘制电量条
+				_ctx.save();
+				_ctx.translate( screenCenterX, screenCenterX );
+				_ctx.rotate( -spaceship.deg * Math.PI / 180 );
+
+				_ctx.beginPath();
+
+				if( spaceship.power > 60 ){
+					_ctx.strokeStyle = powerBar_good;
+				} else if( spaceship.power <= 60 && spaceship.power > 20 ){
+					_ctx.strokeStyle = powerBar_medium;
+				} else{
+					_ctx.strokeStyle = powerBar_bad;
+				}
+
+				_ctx.lineWidth = powerBra_width;
+				_ctx.moveTo( spaceship.orbit, -powerBar_pos_offset);
+				_ctx.lineTo( spaceship.orbit + shipWidth * ( spaceship.power / 100 ), -powerBar_pos_offset );
+				_ctx.stroke();
+
+				// 绘制飞船
+				_ctx.drawImage( oImg, spaceship.orbit, 0, shipWidth, shipWidth );
+				_ctx.restore();
+
+				ctx.clearRect( 0, 0, screenWidth, screenHeight );
+				ctx.drawImage( canPlanet, 0, 0, screenWidth, screenHeight );
+				return true;
+			} catch ( error ){
+				alert( error.message );
+			}
+
+		};
+		oImg.src = './img/min-iconfont-rocket-active.png';
+	};
+
+	/**
+	 * [onDraw 绘制屏幕画布]
+	 * @param  {[arr]} spaceships [飞船队列]
+	 */
 	var onDraw = function( spaceships ){
-		if( !( spaceship === undefined || spaceship.every( function( item ){
+		if( !( spaceships === undefined || spaceships.every( function( item ){
 			return item === undefined;
 		} ) ) ){
 			cacheCtx.clearRect( 0, 0, screenWidth, screenHeight );
 			for (var i = 0; i < spaceships.length; i++) {
 				if( spaceships[i] !== undefined ){
-					drawSpaceship( cacheCtx, spaceship[i] );
+					drawSpaceship( cacheCtx, spaceships[i] );
 				}
 			}
 		} else{
-			cacheCtx.clearRect( 0, 0, screenWidth, screenHeight );
+			ctx.clearRect( 0, 0, screenWidth, screenHeight );
 		}
 	};
 
+	/**
+	 * [animLoop 动画循环]
+	 */
 	var animLoop = function(){
-		onDraw( mediator.getSpaceships );
+		onDraw( mediator.getSpaceships() );
 		requestAnimationFrame( animLoop );
 	};
 
 	/**
-	 * [drawSpaceship description]
-	 * @param  {[obj]} _ctx       [cacheCxt缓存画布对象]
-	 * @param  {[obj]} spaecship [飞船对象]
-	 * @return {[boolean]}           [绘画成功返回true，反之false]
+	 * [setMediator 安装喇叭，用于获取spaceships数组数据渲染动画]
 	 */
-	var drawSpaceship = function( _ctx, spaecship ){
-		var oImg = new Image();
-		oImg.onload = function(){
-			_ctx.save();
-			_ctx.translate( screenCenterX, screenCenterY );
-			_ctx.rotate( -spaceship.deg * Math.PI / 180 );
-			/*画电量条*/
-			if( spaceship.power >= 60 ){
-				_ctx.strokeStyle = '#7DE8B7';
-			} else if( spaceship.power < 60 && spaceship.power >= 20 ){
-				_ctx.strokeStyle = '#FFB22E';
-			} else{
-				_ctx.strokestyle = '#FF3900';
-			}
-			_ctx.lineWidth = 5;
-			_ctx.beginPath();
-			_ctx.moveTo( sapceship.orbit, -powerBar_pos_offset );
-			_ctx.lineTo( spaceship.orbit + 40 * ( sapceship.power / 100 ), -powerBar_pos_offset );
-			_ctx.stroke();
-
-			_ctx.drawImage( oImg, sapceship.orbit, 0, spaceship_size, spaceship_size );
-			_ctx.restore();
-
-			ctx.clearRect( 0, 0, screenWidth, screenHeight );
-			ctx.drawImage( cacheCanva, 0, 0, screenWidth, screenHeight );
-
-		};
-		oImg.src = '../img/min-iconfont-rocket-active.png';
-	};
-
 	var setMediator = function( _mediator ){
 		mediator = _mediator;
 	};
-
-	try{
-
-		(function(){
-
-			var oPlant, ctx;
-
-			oPlant = document.querySelector('.plant');
-			oPlant.width = screenWidth;
-			oPlant.height = screenHeight;
-			ctx = oPlant.getContext( '2d' );
-
-			ctx.clearRect( 0, 0, screenWidth, screenHeight );
-
-			drawPlanet( ctx );
-
-			drawOrbit( ctx );
-			
-		})();
-
-	} catch( e ){
-
-		throw new Error( '您的浏览器不支持Canvas！' );
-
-	}
 
 	return {
 		setMediator: setMediator,
 		animLoop: animLoop
 	};
-	
-})();
+
+} )();
+
+/**
+ * [操作日志输出至控制台]
+ * @return {[obj]}         [抛出show方法]
+ */
+var ConsoleMs = ( function(){
+
+	var oUl = $('.console ul');
+	var show = function( msg ){
+		var oLi = $('<li/>');
+		oLi.text( msg );
+		oUl.prepend( oLi );
+	};
+
+	return {
+		show: show
+	};
+
+} )();
 
 window.onload = function(){
-
 	var commander, mediator;
 	commander = new Commander();			//指挥官实例
-	mediator = new Mediator();				//消息载体(喇叭)
-	mediator.registor( commander );			//给指挥官装个喇叭
-	btnListener( commander );				//监听按钮事件
-	AnimUtil.setMediator( mediator );
-	AnimUtil.animLoop();
+	mediator = new Mediator();				//喇叭实例
+	mediator.registor( commander );			//给指挥官安装喇叭
+	btnListener( commander );				//按钮监听
+	AnimTool.setMediator( mediator );		//给工具安装喇叭，用于获取spaceships数组数据渲染动画
+	AnimTool.animLoop();					//动画循环
 };
+
