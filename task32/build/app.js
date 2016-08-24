263287e94d143a73590b24fe6a9064fe58278047
17khba <17khbaboy@gmail.com>
2016-08-24 14:38:58 +0800

    新新建项目

---
+++ task32/build/app.js
@@ -0,0 +1,114 @@
+'use strict';
+
+require('babel-polyfill');
+
+/* 选择器方法*/
+var $ = function $(selector) {
+    return document.querySelector(selector);
+};
+
+/* 事件绑定兼容*/
+var on = function on(ele, event, fn) {
+    if (ele.addEventListener) {
+        ele.addEventListener(event, fn, false);
+    } else if (ele.attachEvent) {
+        ele.attachEvent('on' + event, fn);
+    } else {
+        ele['on' + event] = fn;
+    }
+};
+
+var tb = $('.tb').tBodies[0];
+var txt = $('.txt');
+var btn = $('.btn');
+
+var direction = ['top', 'right', 'bottom', 'left'];
+
+/* 获取当前元素*/
+var getPos = function getPos(x, y) {
+    return tb.rows[x].cells[y];
+};
+
+var setDiv = function setDiv(ele) {
+    return ele.innerHTML = '<div></div>';
+};
+var clearDiv = function clearDiv(ele) {
+    return ele.innerHTML = '';
+};
+
+/* 通过类名的方式调整方向*/
+var setDir = function setDir(obj, direction) {
+    return obj.className = direction;
+};
+
+/* 默认位置信息*/
+var pos = {
+    currentEle: getPos(5, 5), // 当前元素
+    dir: 3, // 当前方向
+    x: 5, // 当前行号
+    y: 5 };
+
+/* 调整默认方向*/
+var changeDir = function changeDir(num) {
+    pos.dir = (pos.dir + num < 0 ? 3 : pos.dir + 1) % 4;
+    setDir(pos.currentEle, direction[pos.dir]);
+};
+
+/* 初始化*/
+var init = function init(_currentEle) {
+    setDir(pos.currentEle, '');
+    clearDiv(pos.currentEle);
+    setDir(_currentEle, direction[pos.dir]);
+    setDiv(_currentEle);
+    pos.currentEle = _currentEle;
+};
+
+var goPublic = function goPublic(str, num) {
+    if (pos[str] <= 1) return;
+    if (pos[str] > 1) pos[str] = pos[str] + num;
+    var _currentEle = getPos(pos.x, pos.y);
+    init(_currentEle);
+};
+
+var go = function go() {
+    var _className = pos.currentEle.className;
+    switch (_className) {
+        case 'top':
+            goPublic('x', -1);
+            break;
+        case 'right':
+            goPublic('y', 1);
+            break;
+        case 'bottom':
+            goPublic('y', 1);
+            break;
+        case 'left':
+            goPublic('y', -1);
+            break;
+    }
+};
+
+var run = function run() {
+    var txtVal = txt.value.trim().toLowerCase().replace(/\s/, '');
+    switch (txtVal) {
+        case 'go':
+            go();
+            break;
+        case 'tunlef':
+            changeDir(1);
+            break;
+        case 'tunrig':
+            changeDir(-1);
+            break;
+        case 'tunbak':
+            changeDir(2);
+            break;
+        default:
+            break;
+    }
+};
+
+setDir(pos.currentEle, 'left');
+setDiv(pos.currentEle);
+on(btn, 'click', run);
+//# sourceMappingURL=app.js.map
--- task32/js/app.js
+++ task32/js/app.js
@@ -1,13 +1,99 @@
-function $( selector ){
-	return document.querySelector( selector );
-}
+import 'babel-polyfill';
+/* 选择器方法*/
+const $ = selector => document.querySelector(selector);
 
-function on( ele, event, fn ){
-	if( ele.addEventListener ){
-		ele.addEventListener( event, fn, false );
-	} else if( ele.attachEvent ){
-		ele.attachEvent( 'on' + event, fn );
-	} else{
-		ele[ 'on' + event ] = fn;
-	}
-}
+/* 事件绑定兼容*/
+const on = (ele, event, fn) => {
+    if (ele.addEventListener) {
+        ele.addEventListener(event, fn, false);
+    } else if (ele.attachEvent) {
+        ele.attachEvent(`on${event}`, fn);
+    } else {
+        ele[`on${event}`] = fn;
+    }
+};
+
+const tb = $('.tb').tBodies[0];
+const [txt, btn] = [$('.txt'), $('.btn')];
+const direction = ['top', 'right', 'bottom', 'left'];
+
+/* 获取当前元素*/
+const getPos = (x, y) => tb.rows[x].cells[y];
+
+const setDiv = ele =>  ele.innerHTML = `<div></div>`;
+const clearDiv = ele => ele.innerHTML = '';
+
+/* 通过类名的方式调整方向*/
+const setDir = (obj, direction) => obj.className = direction;
+
+/* 默认位置信息*/
+const pos = {
+    currentEle: getPos(5, 5),       // 当前元素
+    dir: 3,                         // 当前方向
+    x: 5,                           // 当前行号
+    y: 5,                           // 当前列号
+};
+
+/* 调整默认方向*/
+const changeDir = num => {
+    pos.dir = (pos.dir + num < 0 ? 3 : pos.dir + 1) % 4;
+    setDir(pos.currentEle, direction[pos.dir]);
+};
+
+/* 初始化*/
+const init = _currentEle => {
+    setDir(pos.currentEle, '');
+    clearDiv(pos.currentEle);
+    setDir(_currentEle, direction[pos.dir]);
+    setDiv(_currentEle);
+    pos.currentEle = _currentEle;
+};
+
+const goPublic = (str, num) => {
+    if(pos[str] <= 1) return;
+    if(pos[str] > 1) pos[str] = pos[str] + num;
+    const _currentEle = getPos(pos.x, pos.y);
+    init(_currentEle);
+};
+
+const go = () => {
+    const _className = pos.currentEle.className;
+    switch (_className) {
+        case 'top':
+            goPublic('x', -1);
+            break;
+        case 'right':
+            goPublic('y', 1);
+            break;
+        case 'bottom':
+            goPublic('y', 1);
+            break;
+        case 'left':
+            goPublic('y', -1);
+            break;
+    }
+};
+
+const run = function () {
+    const txtVal = txt.value.trim().toLowerCase().replace(/\s/, '');
+    switch (txtVal) {
+    case 'go':
+        go();
+        break;
+    case 'tunlef':
+        changeDir(1);
+        break;
+    case 'tunrig':
+        changeDir(-1);
+        break;
+    case 'tunbak':
+        changeDir(2);
+        break;
+    default:
+        break;
+    }
+};
+
+setDir(pos.currentEle, 'left');
+setDiv(pos.currentEle);
+on(btn, 'click', run);
